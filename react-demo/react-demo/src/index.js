fcd0f52ace12efb07acd47ce456903f03081c45b
vishal aradwad <vishal.reddy2551@gmail.com>
2022-11-05 15:26:52 +0530

    CLASS

---
+++ react-demo/react-demo/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const fname = "vishal";
+const currdate = new Date().toLocaleDateString();
+const currtime = new Date().toLocaleTimeString();
+
+ReactDOM.render(
+  <>
+   <h1>{my name is }</h1>
+  </>
+)
+
+
